50ec9a8454d6007ca1ee8f350c0fbbf4bf3fa487
liza314 <54549663+liza314@users.noreply.github.com>
2020-04-14 22:00:18 +0600

    Create times_1409.cpp

---
+++ times_1409.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+ int main()
+ {
+     int h,l;
+     cin>>h;
+     cin>>l;
+     int g=(h+l)-1;
+     cout<<g-h<<"  "<<g-l<<endl;
+     
+     
+ }
+
