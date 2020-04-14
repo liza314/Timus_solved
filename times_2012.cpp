d875e7bb9d0494aa0adeef06a8f5a7a0ec6fd008
liza314 <54549663+liza314@users.noreply.github.com>
2020-04-14 22:58:18 +0600

    Create times_2012.cpp

---
+++ times_2012.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+ using namespace std;
+ int main()
+ {
+     int f;
+     cin>>f;
+     int r=12-f;
+    int tim=r*45;
+     if(tim<=240)
+     { 
+         cout<<"Yes"<<endl;
+         
+     }
+     else{
+         
+        cout<<"No"<<endl;
+     }
+ }
