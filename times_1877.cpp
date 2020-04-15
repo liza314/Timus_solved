881ecbfe9dead6f7c667582fa60b220e8b4c7765
liza314 <54549663+liza314@users.noreply.github.com>
2020-04-15 17:19:40 +0600

    Create times_1877.cpp

---
+++ times_1877.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int a,b;
+   cin>>a>>b;
+   if(a%2==0 || b%2!=0)
+   {
+       cout<<"Yes"<<endl;
+   }
+   else if(a%2!=0 || b%2==0)
+   {
+       cout<<"no"<<endl;
+   }
+    
+}
